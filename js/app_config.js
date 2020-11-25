2c1657561617999cb42b1d7fb9bf37ee197481cd
ntduong10698 <44547589+ntduong10698@users.noreply.github.com>
2020-11-25 15:57:51 +0700

    init

---
+++ js/app_config.js
@@ -0,0 +1,375 @@
+const INFOR_CV_VN = {
+    imgCV: './public/img-cv.jpg',
+    name: 'Nguyễn Trọng Dương',
+    job: 'Software Developer',
+    contacts: [
+        {
+            icon: '<i class="fas fa-mobile-alt"></i>',
+            value: '0966186222',
+            href: 'tel:0966186222'
+        },
+        {
+            icon: '<i class="far fa-envelope"></i>',
+            value: 'ntduong10698@gmail.com',
+            href: 'mailto:ntduong10698@gmail.com'
+        },
+        {
+            icon: '<i class="fab fa-facebook-square"></i>',
+            value: 'facebook.com/duong.nguyentrong.52',
+            href: 'https://www.facebook.com/duong.nguyentrong.52'
+        },
+        {
+            icon: '<i class="fab fa-github"></i>',
+            value: 'github.com/ntduong10698',
+            href: 'https://github.com/ntduong10698'
+        }
+    ],
+    skills: [
+        {
+            icon: '<i class="fab fa-html5"></i>',
+            value: 'HTML'
+        },
+        {
+            icon: '<i class="fab fa-css3-alt"></i>',
+            value: 'CSS'
+        },
+        {
+            icon: '<i class="fab fa-js"></i>',
+            value: 'JavaScript/ Jquery'
+        },
+        {
+            icon: '<i class="fab fa-bootstrap"></i>',
+            value: 'Bootstrap 3.x/4.x'
+        },
+        {
+            icon: '<i class="fab fa-java"></i>',
+            value: 'Java Core/ JSP/ Servlet'
+        },
+        {
+            icon: '<i class="fas fa-leaf"></i>',
+            value: 'Spring Framework'
+        },
+        {
+            icon: '<i class="fas fa-database"></i>',
+            value: 'SQL Server/ MySQL/ MariaDB'
+        }
+    ],
+    descriptionCV: 'Tôi là Nguyễn Trọng Dương là một Web Developer. Tiểu sử các công việc tóm tắt, và lợi ích khi vào công ty.',
+    experiences: [
+        {
+            name: 'Camera Hà Phát',
+            company: 'BKSoftwarevn',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, ipsam dolores nemo rerum veritatis suscipit. Atque quisquam minus sint voluptates repellendus delectus sapiente, esse dolorem possimus sunt a iure voluptatibus!',
+            info: [
+                {
+                    name: 'Project size',
+                    value: '2 people' 
+                },
+                {
+                    name: 'Position/ Responsibilities',
+                    value: 'Developer/ Create prototype, Implement'
+                },
+                {
+                    name: 'Used Technologies',
+                    value: 'MySQL, Spring Framework, HTML, CSS, JS'
+                }
+            ]
+        },
+        {
+            name: 'Camera Hà Phát',
+            company: 'BKSoftwarevn',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, ipsam dolores nemo rerum veritatis suscipit. Atque quisquam minus sint voluptates repellendus delectus sapiente, esse dolorem possimus sunt a iure voluptatibus!',
+            info: [
+                {
+                    name: 'Project size',
+                    value: '2 people' 
+                },
+                {
+                    name: 'Position/ Responsibilities',
+                    value: 'Developer/ Create prototype, Implement'
+                },
+                {
+                    name: 'Used Technologies',
+                    value: 'MySQL, Spring Framework, HTML, CSS, JS'
+                }
+            ]
+        },
+        {
+            name: 'Camera Hà Phát',
+            company: 'BKSoftwarevn',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, ipsam dolores nemo rerum veritatis suscipit. Atque quisquam minus sint voluptates repellendus delectus sapiente, esse dolorem possimus sunt a iure voluptatibus!',
+            info: [
+                {
+                    name: 'Project size',
+                    value: '2 people' 
+                },
+                {
+                    name: 'Position/ Responsibilities',
+                    value: 'Developer/ Create prototype, Implement'
+                },
+                {
+                    name: 'Used Technologies',
+                    value: 'MySQL, Spring Framework, HTML, CSS, JS'
+                }
+            ]
+        },
+        {
+            name: 'Camera Hà Phát',
+            company: 'BKSoftwarevn',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, ipsam dolores nemo rerum veritatis suscipit. Atque quisquam minus sint voluptates repellendus delectus sapiente, esse dolorem possimus sunt a iure voluptatibus!',
+            info: [
+                {
+                    name: 'Project size',
+                    value: '2 people' 
+                },
+                {
+                    name: 'Position/ Responsibilities',
+                    value: 'Developer/ Create prototype, Implement'
+                },
+                {
+                    name: 'Used Technologies',
+                    value: 'MySQL, Spring Framework, HTML, CSS, JS'
+                }
+            ]
+        },
+        {
+            name: 'Camera Hà Phát',
+            company: 'BKSoftwarevn',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, ipsam dolores nemo rerum veritatis suscipit. Atque quisquam minus sint voluptates repellendus delectus sapiente, esse dolorem possimus sunt a iure voluptatibus!',
+            info: [
+                {
+                    name: 'Project size',
+                    value: '2 people' 
+                },
+                {
+                    name: 'Position/ Responsibilities',
+                    value: 'Developer/ Create prototype, Implement'
+                },
+                {
+                    name: 'Used Technologies',
+                    value: 'MySQL, Spring Framework, HTML, CSS, JS'
+                }
+            ]
+        }
+    ],
+    products: [
+        {
+            img: './public/product/haphat.PNG',
+            name: 'Camera Hà Phát',
+            description: 'Website bán camera',
+            href: 'https://haphatsmarthome.com/'
+        },
+        {
+            img: './public/product/quyhoachbacgiang.png',
+            name: 'Quy hoạch sử dụng đất',
+            description: 'Website quản lý qhsdd nội bộ',
+            href: ''
+        },
+        {
+            img: './public/product/nataliepmu.PNG',
+            name: 'Natalie PMU',
+            description: 'Website dịch vụ thẩm mỹ',
+            href: 'https://nataliepmu.com/'
+        },
+        {
+            img: './public/product/theitbidienavhh.PNG',
+            name: 'AVHH',
+            description: 'Thiết bị điện AVHH',
+            href: 'https://thietbidienavhh.com/'
+        },
+        {
+            img: './public/product/dongydomuoi.PNG',
+            name: 'Đỗ Mười',
+            description: 'Đông Y Đỗ Mười',
+            href: 'http://dongydomuoi.com/'
+        }
+    ]
+}
+
+const INFOR_CV_EN = {
+    imgCV: './public/img-cv.jpg',
+    name: 'Nguyen Trong Duong',
+    job: 'Software Developer',
+    contacts: [
+        {
+            icon: '<i class="fas fa-mobile-alt"></i>',
+            value: '0966186222',
+            href: 'tel:0966186222'
+        },
+        {
+            icon: '<i class="far fa-envelope"></i>',
+            value: 'ntduong10698@gmail.com',
+            href: 'mailto:ntduong10698@gmail.com'
+        },
+        {
+            icon: '<i class="fab fa-facebook-square"></i>',
+            value: 'facebook.com/duong.nguyentrong.52',
+            href: 'https://www.facebook.com/duong.nguyentrong.52'
+        },
+        {
+            icon: '<i class="fab fa-github"></i>',
+            value: 'github.com/ntduong10698',
+            href: 'https://github.com/ntduong10698'
+        }
+    ],
+    skills: [
+        {
+            icon: '<i class="fab fa-html5"></i>',
+            value: 'HTML'
+        },
+        {
+            icon: '<i class="fab fa-css3-alt"></i>',
+            value: 'CSS'
+        },
+        {
+            icon: '<i class="fab fa-js"></i>',
+            value: 'JavaScript/ Jquery'
+        },
+        {
+            icon: '<i class="fab fa-bootstrap"></i>',
+            value: 'Bootstrap 3.x/4.x'
+        },
+        {
+            icon: '<i class="fab fa-java"></i>',
+            value: 'Java Core/ JSP/ Servlet'
+        },
+        {
+            icon: '<i class="fas fa-leaf"></i>',
+            value: 'Spring Framework'
+        },
+        {
+            icon: '<i class="fas fa-database"></i>',
+            value: 'SQL Server/ MySQL/ MariaDB'
+        }
+    ],
+    descriptionCV: 'Tôi là Nguyễn Trọng Dương là một Web Developer. Tiểu sử các công việc tóm tắt, và lợi ích khi vào công ty.',
+    experiences: [
+        {
+            name: 'Camera Hà Phát',
+            company: 'BKSoftwarevn',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, ipsam dolores nemo rerum veritatis suscipit. Atque quisquam minus sint voluptates repellendus delectus sapiente, esse dolorem possimus sunt a iure voluptatibus!',
+            info: [
+                {
+                    name: 'Project size',
+                    value: '2 people' 
+                },
+                {
+                    name: 'Position/ Responsibilities',
+                    value: 'Developer/ Create prototype, Implement'
+                },
+                {
+                    name: 'Used Technologies',
+                    value: 'MySQL, Spring Framework, HTML, CSS, JS'
+                }
+            ]
+        },
+        {
+            name: 'Camera Hà Phát',
+            company: 'BKSoftwarevn',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, ipsam dolores nemo rerum veritatis suscipit. Atque quisquam minus sint voluptates repellendus delectus sapiente, esse dolorem possimus sunt a iure voluptatibus!',
+            info: [
+                {
+                    name: 'Project size',
+                    value: '2 people' 
+                },
+                {
+                    name: 'Position/ Responsibilities',
+                    value: 'Developer/ Create prototype, Implement'
+                },
+                {
+                    name: 'Used Technologies',
+                    value: 'MySQL, Spring Framework, HTML, CSS, JS'
+                }
+            ]
+        },
+        {
+            name: 'Camera Hà Phát',
+            company: 'BKSoftwarevn',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, ipsam dolores nemo rerum veritatis suscipit. Atque quisquam minus sint voluptates repellendus delectus sapiente, esse dolorem possimus sunt a iure voluptatibus!',
+            info: [
+                {
+                    name: 'Project size',
+                    value: '2 people' 
+                },
+                {
+                    name: 'Position/ Responsibilities',
+                    value: 'Developer/ Create prototype, Implement'
+                },
+                {
+                    name: 'Used Technologies',
+                    value: 'MySQL, Spring Framework, HTML, CSS, JS'
+                }
+            ]
+        },
+        {
+            name: 'Camera Hà Phát',
+            company: 'BKSoftwarevn',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, ipsam dolores nemo rerum veritatis suscipit. Atque quisquam minus sint voluptates repellendus delectus sapiente, esse dolorem possimus sunt a iure voluptatibus!',
+            info: [
+                {
+                    name: 'Project size',
+                    value: '2 people' 
+                },
+                {
+                    name: 'Position/ Responsibilities',
+                    value: 'Developer/ Create prototype, Implement'
+                },
+                {
+                    name: 'Used Technologies',
+                    value: 'MySQL, Spring Framework, HTML, CSS, JS'
+                }
+            ]
+        },
+        {
+            name: 'Camera Hà Phát',
+            company: 'BKSoftwarevn',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, ipsam dolores nemo rerum veritatis suscipit. Atque quisquam minus sint voluptates repellendus delectus sapiente, esse dolorem possimus sunt a iure voluptatibus!',
+            info: [
+                {
+                    name: 'Project size',
+                    value: '2 people' 
+                },
+                {
+                    name: 'Position/ Responsibilities',
+                    value: 'Developer/ Create prototype, Implement'
+                },
+                {
+                    name: 'Used Technologies',
+                    value: 'MySQL, Spring Framework, HTML, CSS, JS'
+                }
+            ]
+        }
+    ],
+    products: [
+        {
+            img: './public/product/haphat.PNG',
+            name: 'Camera Hà Phát',
+            description: 'Website bán camera',
+            href: 'https://haphatsmarthome.com/'
+        },
+        {
+            img: './public/product/quyhoachbacgiang.png',
+            name: 'Quy hoạch sử dụng đất',
+            description: 'Website quản lý qhsdd nội bộ',
+            href: ''
+        },
+        {
+            img: './public/product/nataliepmu.PNG',
+            name: 'Natalie PMU',
+            description: 'Website dịch vụ thẩm mỹ',
+            href: 'https://nataliepmu.com/'
+        },
+        {
+            img: './public/product/theitbidienavhh.PNG',
+            name: 'AVHH',
+            description: 'Thiết bị điện AVHH',
+            href: 'https://thietbidienavhh.com/'
+        },
+        {
+            img: './public/product/dongydomuoi.PNG',
+            name: 'Đỗ Mười',
+            description: 'Đông Y Đỗ Mười',
+            href: 'http://dongydomuoi.com/'
+        }
+    ]
+}
---
+++ js/main.js
@@ -0,0 +1,158 @@
+var btnExportCV, btnDetailCV, imgFlag;
+var imgCV, nameCV, jobCV, eContactTemp, listContact, eSkillTemp, listSkill, descriptionCV, eExperienceTemp, listExperience, listProduct, eProductTemp;
+var langPage = "vn";
+$(function() {
+
+  btnExportCV = $("#btn-export-cv");
+  btnDetailCV = $("#btn-detail-cv");
+  imgFlag = $(".img-flag");
+
+  //element content
+  imgCV = $(".img-cv");
+  nameCV = $(".name-cv");
+  jobCV = $(".job-cv");
+  eContactTemp = $("#e-contact-temp");
+  listContact = $(".list-contact");
+  eSkillTemp = $("#e-skill-temp");
+  listSkill = $(".list-skill");
+  descriptionCV = $(".description-cv");
+  eExperienceTemp = $("#e-experience-temp");
+  listExperience = $(".list-experiences .row");
+  listProduct = $(".list-product .row");
+  eProductTemp = $("#e-product-temp");
+
+  //run particle
+  particlesJS.load('particles-js', '../plugins/particles/particles.json');
+
+  //view content
+  console.log(INFOR_CV_VN);
+  viewContentCV(INFOR_CV_VN);
+
+  //tool page
+  clickBtnExportCV();
+  clickBtnDetailCV();
+  clickImgFlag();
+})
+
+function clickBtnExportCV() {
+  btnExportCV.click(function() {
+    let printElemnt = $("main").clone();
+    printElemnt.find(".product").remove();
+    printElemnt.printThis({
+      importCSS: true,
+      importStyle: true,
+      loadCSS: "./style.css",
+    })
+  })
+}
+
+function clickBtnDetailCV() {
+  btnDetailCV.click(function() {
+    window.open("./public/CVNguyenTrongDuong_EN.docx", "_blank");
+  })
+}
+
+function clickImgFlag() {
+  imgFlag.click(function() {
+      let lang = $(this).attr("data-lang");
+      if(lang != langPage) {
+        langPage = lang;
+        if(langPage == 'vn') {
+          //set tool
+          btnDetailCV.find("span").html("Chi tiết CV");
+          btnExportCV.find("span").html("Xuất CV");
+          //set content
+          viewContentCV(INFOR_CV_VN);
+        } else if(langPage == 'en') {
+          //set tool
+          btnDetailCV.find("span").html("Detail CV");
+          btnExportCV.find("span").html("Export CV");
+          //set content
+          viewContentCV(INFOR_CV_EN);
+        }
+      }
+  })
+}
+
+function viewField(text) {
+  return text ? text : "";
+}
+
+function viewContentCV(inforCV) {
+  imgCV.attr("src", viewField(inforCV.imgCV));
+  imgCV.attr("alt", viewField(inforCV.name));
+  nameCV.html(viewField(inforCV.name));
+  jobCV.html(viewField(inforCV.job));
+  
+  //view contact
+  let {contacts} = inforCV;
+  if(contacts) {
+    let viewContacts = contacts.map(data => {
+      let eContactClone = eContactTemp.clone().removeAttr("id").removeClass("d-none");
+      eContactClone.find("a").attr("href", viewField(data.href));
+      eContactClone.find(".icon-contact").html(viewField(data.icon));
+      eContactClone.find(".val-contact").html(viewField(data.value));
+      return eContactClone;
+    })
+    listContact.html(viewContacts);
+    listContact.append(eContactTemp);
+  }
+
+  //view skill
+  let {skills} = inforCV;
+  if(skills) {
+    let viewSkills = skills.map(data => {
+      let eSkillClone = eSkillTemp.clone().removeAttr("id").removeClass("d-none");
+      eSkillClone.find(".icon-skill").html(viewField(data.icon));
+      eSkillClone.find(".val-skill").html(viewField(data.value));
+      return eSkillClone;
+    })
+    listSkill.html(viewSkills);
+    listSkill.append(eSkillTemp);
+  }
+
+  //view description-cv
+  descriptionCV.html(viewField(inforCV.descriptionCV));
+
+  //view experiences
+  let {experiences} = inforCV;
+  if(experiences) {
+    let viewExperiences = experiences.map(data => {
+      let eExperienceClone = eExperienceTemp.clone().removeAttr("id").removeClass("d-none");
+      eExperienceClone.find(".name-experience").html(viewField(data.name));
+      eExperienceClone.find(".company-experience").html(viewField(data.company));
+      eExperienceClone.find(".desc-experience").html(viewField(data.description));
+      let liNoteExperienceTemp = eExperienceClone.find("li");
+      let {info} = data;
+      if(info) {
+        let viewNote = info.map(el => {
+          let liNoteExperienceClone = liNoteExperienceTemp.clone();
+          liNoteExperienceClone.find(".name-note-experience").html(viewField(el.name) + ':');
+          liNoteExperienceClone.find(".val-note-experience").html(viewField(el.value));
+          return liNoteExperienceClone;
+        });
+        eExperienceClone.find(".note-experience").html(viewNote);
+      }
+      return eExperienceClone;
+    })
+    listExperience.html(viewExperiences);
+    listExperience.append(eExperienceTemp);
+  }
+
+  //view demo
+  let {products} = inforCV;
+  if(products) {
+    let viewProducts = products.map(data => {
+      let eProductClone = eProductTemp.clone().removeAttr("id").removeClass("d-none");
+      eProductClone.find("a").attr("href", viewField(data.href));
+      let img = eProductClone.find("img");
+      img.attr("src", viewField(data.img));
+      img.attr("alt", viewField(data.name));
+      eProductClone.find(".name-product").html(viewField(data.name));
+      eProductClone.find(".desc-product").html(viewField(data.description));
+      return eProductClone;
+    })
+    listProduct.html(viewProducts);
+    listProduct.append(eProductTemp);
+  }
+}
